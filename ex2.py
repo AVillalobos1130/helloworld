9196734761ddf465904e5664a07755e731634b38
AVillalobos1130 <43752320+AVillalobos1130@users.noreply.github.com>
2018-10-22 09:33:49 -0400

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, to clarify the logic of your program later
+# anything with a # is a comment and is ignored when compiling and running
+
+print " I could have code like this. "
+
+#comments can disable code:
+#print " this will not run."
+print " This will run. " 
