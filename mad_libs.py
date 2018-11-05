3ec5165ecbc500544998aaa9477e741e5f1e96bf
AVillalobos1130 <43752320+AVillalobos1130@users.noreply.github.com>
2018-11-05 09:21:46 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,59 @@
+name = input("Enter a noun:")
+adj = input("Enter an adjective:")
+noun = input("Enter a noun:")
+print("Please excuse " + name + " who is far too " + adj + " to attend " + noun + ".")
+
+name = input("Enter a name:")
+body_part  =input("Enter a part of the body:")
+fluid = input("Enter a type of fluid:")
+substance = input("Enter a type of substance:")
+print (name + " is sick with the "  + body_part + " flu. Drink more "  + fluid + " and take " + substance
+                + "as needed.")
+
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+noun = input("Enter a noun: ")
+mad_lib3 = (name + " is authorized to be at " + place + " instead of " + noun + " class.")
+print(name + " is authorized to be at " + place + " instead of " + noun + " class.")
+
+name = input("Enter a name: ")
+noun = input("Enter a noun: ")
+event = input("Enter an event: ")
+print(name + " is too cool for " + noun + " class. Instead, he will  be attending " + event)
+
+adj = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adj2 = input("Eneter an adjective: ")
+
+print("Every " + adj + " spy  needs a pair of X-ray " + ", an invisible " + noun + ", and a spy mobile equipped with"
+                                                                                   "the most " + adj2 + " features")
+
+adj = input("Enter an adjective: ")
+nationality = input("Enter a nationality: ")
+person = input("Enter the name of a person: ")
+print("Pizza was invented by a " + adj + " " + nationality + " named " + person)
+
+company = input("Enter a company name: ")
+product = input("Enter a product: ")
+group = input("Enter the name of a group: ")
+problem = input("Enter a problem: ")
+print("My company, " + company + ", is developing " + product + " to help " + group + " with " + problem)
+
+name = input("Enter a name: ")
+verb_pt = input("Enter a past-tense verb: ")
+family_member = input("Enter a family member: ")
+verb = input("Enter a verb:")
+print("You! My name is " + name + ", you " + verb_pt + " my " + family_member + " prepare to " + verb)
+
+family_member = input("Enter a family member: ")
+noun = input("Enter a noun: ")
+adj = input("Enter an adjective: ")
+noun2 = input("Enter a noun: ")
+print("Yesterday, my " + family_member + " and I went to the " + noun + " park. There we saw a " + adj + " " + noun2
+      + " on a bike")
+
+noun = input("Enter a noun: ")
+girls_name = input("Enter a girl's name: ")
+adj = input("Enter an adjective: ")
+print("There was once a young " + noun + " named Cinderella, well actually her name was " + girls_name + " but her"
+        + adj + " stepsisters called her that because she was dirty.")
